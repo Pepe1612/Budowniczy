c1e7a3e4f72925fa22c705201c61c0eee4179b73
Patryk <patrykpiecuch16@gmail.com>
2019-11-09 16:12:41 +0100

    Added adding coordinates through consoles in Ludzik.java

--- src/me/pepe/Ludzik.java
+++ src/me/pepe/Ludzik.java
@@ -1,4 +1,22 @@
 package me.pepe;
 
+import java.util.Scanner;
+
 public class Ludzik {
+
+    private int xLudzika, yLudzika;
+
+    void setWspolrzedneLudzika(){
+        Scanner odpowiedz = new Scanner(System.in);
+        System.out.print("Podaj x ludzika:");
+        this.xLudzika = odpowiedz.nextInt();
+        System.out.print("Podaj y ludzika:");
+        this.yLudzika = odpowiedz.nextInt();
+    }
+
+    @Override
+    public String toString() {
+        return Integer.toString(this.xLudzika) +
+                " " +Integer.toString(this.yLudzika);
+    }
 }
