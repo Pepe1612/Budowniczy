35ac1d175ba1fdde96628fa2badd0a6712bc1a50
Patryk <patrykpiecuch16@gmail.com>
2019-11-10 19:49:38 +0100

    Final version

--- src/me/pepe/Main.java
+++ src/me/pepe/Main.java
@@ -3,7 +3,7 @@ package me.pepe;
 public class Main {
 
     public static void main(String[] args) {
-        Plansza plansza = new Plansza(3);
+        Plansza plansza = new Plansza(5);
         Ludzik ludzik = new Ludzik(0, 2, plansza);
         plansza.ustawPrzeszkode(1, 1);
         ludzik.idzPrawo();
@@ -13,5 +13,6 @@ public class Main {
         System.out.println("Przeszkody:" + ludzik.getPrzeszkodyLudzika());
         System.out.println("x:" + ludzik.getxLudzika());
         System.out.println("y:" + ludzik.getyLudzika());
+        System.out.println("koniec");
     }
 }
