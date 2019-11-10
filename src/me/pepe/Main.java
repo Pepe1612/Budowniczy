9bfab9c13abfe6af55e383942c3e40c7da141126
Pepe1612 <57411460+Pepe1612@users.noreply.github.com>
2019-11-10 19:51:39 +0100

    Update Main.java

--- src/me/pepe/Main.java
+++ src/me/pepe/Main.java
@@ -14,5 +14,6 @@ public class Main {
         System.out.println("x:" + ludzik.getxLudzika());
         System.out.println("y:" + ludzik.getyLudzika());
         System.out.println("koniec");
+        System.out.println("koniec123");
     }
 }
